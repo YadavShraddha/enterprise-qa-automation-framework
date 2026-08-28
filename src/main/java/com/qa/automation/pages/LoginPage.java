c7b30c6cadb8feab82ac7837eeef9b56ec5c3864
Shraddha Yadav <shraddha.yadav176@gmail.com>
2026-08-28 11:39:53 -0700

    Added Page Object Model for login workflow

---
+++ src/main/java/com/qa/automation/pages/LoginPage.java
@@ -0,0 +1,45 @@
+package com.qa.automation.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By userNamefield = By.name("username");
+    private By passWordfield = By.name("password");
+    private By loginButton = By.xpath("//button[@type = 'submit']");
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void enterUsername(String userName){
+        var element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(userNamefield));
+        element.clear();
+        element.sendKeys(userName);
+    }
+
+    public void enterPassword(String passWord){
+        var element = wait.until(ExpectedConditions.visibilityOfElementLocated(passWordfield));
+        element.clear();
+        element.sendKeys(passWord);
+    }
+
+    public void clickLogin(){
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+    }
+
+    public void login(String userName, String passWord){
+        enterUsername(userName);
+        enterPassword(passWord);
+        clickLogin();
+    }
+}
--- src/test/java/com/qa/automation/tests/LoginTest.java
+++ src/test/java/com/qa/automation/tests/LoginTest.java
@@ -2,9 +2,14 @@ package com.qa.automation.tests;
 import com.qa.automation.config.ConfigReader;
 
 import com.qa.automation.config.ConfigReader;
+import com.qa.automation.pages.LoginPage;
 import com.qa.automation.tests.base.BaseTest;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest {
     @Test
@@ -13,4 +18,20 @@ public class LoginTest extends BaseTest {
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle,"OrangeHRM");
     }
+
+    @Test
+    public void verifyValidLogin(){
+        driver.get(ConfigReader.getProperty("baseUrl"));
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.login(ConfigReader.getProperty("username"),
+                ConfigReader.getProperty("password"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("dashboard"));
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("dashboard"),
+                "login was not successful");
+    }
+
 }
