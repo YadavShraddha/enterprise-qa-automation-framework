06d020893514633910ca5462714c1bd26738b502
Shraddha Yadav <shraddha.yadav176@gmail.com>
2026-09-03 16:32:02 -0700

    Added data-driven negative login tests

--- src/test/java/com/qa/automation/tests/LoginTest.java
+++ src/test/java/com/qa/automation/tests/LoginTest.java
@@ -6,6 +6,7 @@ import com.qa.automation.pages.LoginPage;
 import com.qa.automation.tests.base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 public class LoginTest extends BaseTest {
     @Test
@@ -17,23 +18,30 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void verifyValidLogin(){
-
         loginPage.login(ConfigReader.getProperty("username"),
                 ConfigReader.getProperty("password"));
 
         DashboardPage dashboardPage = new DashboardPage(driver);
-
         Assert.assertTrue(dashboardPage.isDashboardDisplayed(),
                 "dashboard was not displayed after successful login");
     }
 
-    @Test
-    public void verifyInvalidLogin(){
+    @Test(dataProvider = "invalidLoginData")
+    public void verifyInvalidLogin(String username, String password){
 
-        loginPage.login("InvalidUser","InvalidPassword");
-
+        loginPage.login(username,password);
         Assert.assertEquals(loginPage.getInvalidCredentialMessage(),"Invalid credentials",
                 "Expected invalid login error message was not displayed");
+    }
+
+    @DataProvider(name = "invalidLoginData")
+    public Object[][] getInvalidLoginData(){
+        return new Object[][]{
+                {"InvalidUser","InvalidPassword"},
+                {"Admin","WrongPassword"},
+                {"WrongUser", "admin123"},
+//                {"",""}
+        };
     }
 
 }
